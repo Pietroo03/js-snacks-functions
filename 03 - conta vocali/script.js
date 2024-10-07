dcd6e1ad088e4fec49745e463d080f7a3d364a95
Pietroo03 <pietro.ponte.03@gmail.com>
2024-10-07 16:24:07 +0200

    complete exercise 3

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,9 +4,21 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+function number_of_vowel () {
+    let count = 0
+    let vowels = []
+    for (let i = 0; i < word.length; i++) {
+        if (word.charAt(i) == 'a' || word.charAt(i) == 'e' || word.charAt(i) == 'i' || word.charAt(i) == 'o' || word.charAt(i) == 'u') {
+            count++
+            vowels.push(word.charAt(i))
+        }
+    }
+    const output_function = `${word} : ${count} (${vowels})`
+    console.log(output_function);
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+number_of_vowel()
 
 
 
