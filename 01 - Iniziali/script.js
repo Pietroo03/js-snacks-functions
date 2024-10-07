cbc5b5e29260635a245d10c87fffbeb32553efd3
Pietroo03 <pietro.ponte.03@gmail.com>
2024-10-07 15:28:28 +0200

    complete exercise 1

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -2,12 +2,18 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
-
+function take_first_letter () {
+    let capital_letters = []
+    for (let i = 0; i < names.length; i++) {
+        let single_name = names[i]  
+        capital_letters.push(single_name.charAt(0))
+    }
+    console.log(capital_letters);
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+take_first_letter()
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
