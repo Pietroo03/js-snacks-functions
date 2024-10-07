007a8c479c803230370188b4c009adb32294332d
Pietroo03 <pietro.ponte.03@gmail.com>
2024-10-07 16:33:32 +0200

    complete exercise 4

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -1,13 +1,19 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const name = 'Mario';
+const oneName = 'Mario';
 
 
 // Dichiara la funzione qui.
+function say_hi_to () {
+    let hi = `Ciao ${oneName}`
+    if (oneName.length != 0) {
+        console.log(hi);
+    }
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+say_hi_to()
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
