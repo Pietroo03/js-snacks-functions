d390a7e6de945d65b1481af3678e90dd9d4bf9e4
Pietroo03 <pietro.ponte.03@gmail.com>
2024-10-07 17:24:22 +0200

    modified exercise 2

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -4,19 +4,18 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-function array_names_start_with_a() {
-    const array_function = []
-    for (let i = 0; i < names.length; i++) {
-        if (names[i].charAt(0) === 'A') {
-            array_function.push(names[i])
+function array_names_start_with_a(array, letter) {
+    const filtered_names = []
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].charAt(0) === letter) {
+            filtered_names.push(array[i])
         }
     }
-    console.log(array_function);
-    
+    console.log(filtered_names); 
 }
 
 // Invoca la funzione qui e stampa il risultato in console
-array_names_start_with_a()
+array_names_start_with_a(names, 'A')
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
